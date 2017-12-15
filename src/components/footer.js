a4062cb0fb3bf20c3efd3d4b8f50245e28c1ae52
Erick.S <esatire@gmail.com>
2017-12-15 08:20:12 +0200

    hi

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+    render() {
+        return <footer>
+            <span class="poofoo">Hello I am the footer!</span>
+        </footer>;
+    }
+}
+
