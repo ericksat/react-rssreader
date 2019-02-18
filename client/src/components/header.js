2a6bead388b2406fb793a2459be956fc8ba82244
Erick.S <esatire@gmail.com>
2019-02-18 23:01:47 +0200

    * Header tweaks

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default class Header extends React.Component {
     render() {
         return (<header className="App__header">
-            <button className="btn settings-btn" onClick={this.props.onMenu}>
+            <button className="settings-btn" onClick={this.props.onMenu}>
                 <i className="fas fa-bars"></i>
             </button>
             <h1 className="App__title">
