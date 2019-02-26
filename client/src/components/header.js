4f724d8582a59f3bab27e5f03aa35487c20fcb42
Erick.S <esatire@gmail.com>
2019-02-26 21:59:26 +0200

    * Small tweaks for Google's Lighthouse

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default class Header extends React.Component {
     render() {
         return (<header className="App__header">
-            <button className="settings-btn" onClick={this.props.onMenu}>
+            <button className="settings-btn" onClick={this.props.onMenu} aria-label="Toggle Site-List sidebar">
                 <i className="fas fa-bars"></i>
             </button>
             <h1 className="App__title">
